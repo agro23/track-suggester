84d564af62d513ffcd5a43cb82e0a1f8878d8d4d
Andy Grossberg <andy.grossberg@gmail.com>
2018-01-12 10:39:45 -0800

    Add form clear on submit and clean code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-var result = "RESULTS";
 
-//Business logic
+// Variables
+var result = "RESULTS";
+var flavor ="none";
+var question1 = "";
+var question2 = "";
+var question3 = "";
+var question4 = "";
+var question5 = "";
 
   $("form#survey").submit(function(e) {
     e.preventDefault();
@@ -11,9 +17,27 @@ var result = "RESULTS";
     console.log("got the results, I think.")
     result =  result + "";
 
+    question1 = parseInt( $("input:radio[name=question1]:checked").val() );
+    question2 = $("input:radio[name=question2]:checked").val();
+    question3 = $("input:radio[name=question3]:checked").val();
+    question4 = $("input:radio[name=question4]:checked").val();
+    question5 = $("input:radio[name=question5]:checked").val();
+    // console.log("flavor is: " + flavor);
+    console.log("question 1 is: " + question1);
+    console.log("question 3 is: " + question3);
+
+    // $(input:radio[name=question5]).attr("checked" , false );
+    // x = $("input:radio[name=question5]:checked").val();
+
+    $('#survey').trigger("reset");
+
+    // UI logic
+      // $("#results").text(result);
+      // $("#results").append(flavor);
+      // console.log("now flavor is: " + flavor)
+
   });
 
-// UI logic
-  $("results").text(result);
+
 
 });
